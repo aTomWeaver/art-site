30f2dc40231e027ae7377578e9a7bc0deda3b055
aTomWeaver <atomweavermail@gmail.com>
2024-06-19 15:11:06 -0400

    initialize site

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+    return (
+        <div>
+            <h1>Hey there</h1>
+            <p>This is just the test page to make sure routing is working as expected.</p>
+            <p>Carry on c:</p>
+        </div>
+    )
+}
